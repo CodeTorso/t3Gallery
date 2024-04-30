1bb9d9bd5e94bfa1db2e3f6c882ab741c48d2fa1
codetorso <pruthvirajthinks@gmail.com>
2024-04-30 22:08:52 +0530

    database created and attached

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/07f89e79-31e5-4f4f-8137-e3532f6217ed-fpzk4r.png",
   "https://utfs.io/f/8015c9cc-e4ab-4644-b5e7-59c4f2f957dc-jqrlow.png"
 ]
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
     <main className="bg-slate-800 h-screen">
       <div className="flex flex-wrap gap-4 p-2">
@@ -18,7 +20,12 @@ export default function HomePage() {
         }
       </div>
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        Hello there
+        {posts.map((v, i)=> (
+          <div key={i}>
+            <div>{v.name}</div>
+            <div>{`${v.createdAt.getDate()}-${v.createdAt.getMonth()}-${v.createdAt.getFullYear()}`}</div>
+          </div>
+        ))}
 
       </div>
     </main>
